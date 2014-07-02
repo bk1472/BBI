28b540730c13b43e65c9cf41552dc1aa7cbf8b28
Baekwon.choi <bk1472@gmail.com>
2014-07-02 16:44:17 +0900

    1. start interpreter project (add main source file)

---
+++ bbi.cpp
@@ -0,0 +1,29 @@
+/*
+ * bbi.cpp
+ *
+ *  Created on: 2014. 7. 2.
+ *      Author: baekwon.choi
+ */
+#include <iostream>
+using namespace std;
+
+void convert()
+{
+	cout << "convert OK." << endl;
+}
+
+void execute()
+{
+	cout << "execute OK." << endl;
+}
+
+int main (void)
+{
+	convert();
+	execute();
+
+	return 0;
+}
+
+
+
